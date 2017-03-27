c1dc09e57649c5ad24fe3968ae70d0a734a0d2fe
Nick Wichert <nick.wichert@icloud.com>
2017-03-26 22:11:22 -0500

    feel'n good

--- app/app.component.js
+++ app/app.component.js
@@ -15,7 +15,7 @@ var AppComponent = (function () {
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: '<h1>Skeleton Project</h1>'
+            template: ''
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
